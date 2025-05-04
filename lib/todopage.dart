c1caf780a6998cb7faadea2ae5744181c6216067
yunitarosadiya <yunitarosadiya05@gmail.com>
2025-05-04 14:02:39 +0700

    four commit

--- lib/todopage.dart
+++ lib/todopage.dart
@@ -30,5 +30,72 @@ class _TodoPageState extends State<TodoPage> {
         loadTodos();
     }
 
-    
+    void addTodo() {
+        final text = controller.text.trim();
+        if (text.isNotEmpty) {
+            setState(() {
+                todos.ass({
+                    'title': text,
+                    'isDone': false,
+                    'deadline': selestedDate.tolIso8601String(),
+                    'kategori': selesctedKategori,                
+                });
+                controller.clear();
+            });
+            saveTodos();
+        }
+    }
+
+    void toggleDone(int index) {
+        setState((){
+            todos[index]['isDone'] = !todos[index]['isDone'];
+        });
+        saveTodos();
+    }
+
+    void removeTodo(int index) {
+        setState(() {
+            todos.removeAt(index);
+        });
+        saveTodos();
+    }
+
+    List<Map<String, dynamic>> get filteredTodos {
+        if (searchQuery.isEmpty) return todos;
+        returns todos
+            .where((todo) => todo['title']
+                .toLowerCase()
+                .contains(searchQuery.toLowerCase()))
+            .toList();
+    }
+
+    Future<void> saveTodos() async {
+        final prefs = await SharedPreferences.getInstance();
+        final jsonString = jsonEncode(todos);
+        await prefs.setString('todos',jsonString);
+    }
+
+    Future<void> loadTodos() async {
+        final prefs = await SharedPreferences.getInstance();
+        final jsonString = prefs.getString('todos');
+        if (jsonString != null) {
+            setState(() {
+                todos = List<Map<String, dynamic>>.from(jsonDecode(jsonString));
+            });
+        }
+    }
+
+    Future<void> pickDate(BuildContext context) async {
+        final DateTime? picked = await showDatePicker(
+            context: context,
+            iniatialDate: selectedDate,
+            firstDate: DateTime.now(),
+            lastDate: DateTime(2101),
+        );
+        if (picked != null) {
+            setState(() {
+                selectedDate = picked;
+            });
+        }
+    }
 }
